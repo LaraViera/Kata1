bc7003083f316e6363500832861e31555480e7cc
la_mi <la_mi@DESKTOP-0SC39IM.home>
2017-10-18 19:18:24 +0100

    Segunda versión KATA 1.
    Cambiamos el tipo DATE por GregorianCalendar.

--- src/kata1/KATA1.java
+++ src/kata1/KATA1.java
@@ -1,12 +1,17 @@
 package kata1;
  
+import java.util.Calendar;
  import java.util.Date;
+import java.util.GregorianCalendar;
  
  public class KATA1 {
  
-     
      public static void main(String[] args) {
-         Person person= new Person("Lara",new Date(89,11,15));
+         Calendar date;
+         date = GregorianCalendar.getInstance();
+         date.set(1989,11,15);
+         
+         Person person= new Person("Lara",date);
          System.out.println(person.getName()+" tiene "+ person.getAge()+ " años");
          
      }
